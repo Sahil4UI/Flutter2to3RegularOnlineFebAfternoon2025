2fde0170737c530defb2f06d14627ea0ef1b331d
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2025-02-13 14:51:59 +0530

    Create three.dart

---
+++ three.dart
@@ -0,0 +1,55 @@
+void main() {
+  // String msg = "Hello Students How are you?";
+  // multi line String
+  // String msg = '''
+// hello
+// Welcome to Flutter Class
+// Build Apps and Have Fun
+// ''';
+  // print(msg);
+  // double value = 34.567556456;
+  // print(value.toStringAsFixed(0));
+  // print(value.toStringAsFixed(2));
+
+  String first_name = "Sahil";
+  String last_name = "Kumar";
+  // print(first_name.toLowerCase());
+  // print(first_name.toUpperCase());
+  // print(first_name.toUpperCase() + last_name.toUpperCase());
+  // ASCII - American Standard Code for Information Interchange
+  // print(first_name.codeUnitAt(0));
+  // print(first_name.codeUnits);
+  // print(first_name.contains("Sa"));
+  // print(first_name.startsWith("hi"));
+  // print(first_name.endsWith("Sa"));
+  // print("firstName = $first_name");
+  // print(first_name.indexOf("a"));
+
+  // String x = "hey";
+  // String y = "Hey";
+  // jin strings ko hum compare krrhe hai vo equal hai,to result 0
+  // H-h 72-104
+  // -1 means x chotta hai y se
+  // +1 means x bada hai y se
+  // print(x.compareTo(y));
+
+  // String x = "Flutter is used to build cross platform apps";
+  // print(x.split(" "));
+
+  // List x = ["Welcome", "to", "flutter", "Development"];
+  // print(x.join(" "));
+
+  String res = "          hey          ";
+  // print(res.trimLeft()); //it will remove leading spaces
+  // print(res.trimRight()); //it will remove trailing spaces
+  // print(res.trim());
+  // it will remove leading and trailing spaces
+
+  // String a = "hey";
+  // print(a.length);
+
+  // String is immutable
+  String x = "hello";
+  print(x.toUpperCase());
+  print(x);
+}
